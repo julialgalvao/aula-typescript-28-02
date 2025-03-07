d3d64c3369daf71c16ff5f170b6beb0ce6815ef7
julialgalvao <julialgalvao@gmail.com>
2025-03-06 21:54:43 -0300

    Configuração inicial do projeto TypeScript

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let mensagem: string = "Olá, TypeScript!";
+console.log(mensagem);
+
+//npx tsc
+//Isso criará um arquivo dist/index.js.
+//Executar o código JavaScript gerado
+//node dist/index.js
+//Se tudo estiver correto, a saída será:
